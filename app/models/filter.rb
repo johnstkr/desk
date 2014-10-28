fba9d463b009f35ed69322817ed22ee4485c0671
Kelly Johnston <johnstkr@gmail.com>
2014-10-28 15:11:22 -0600

    Feature: Filter.find

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,2 +1,21 @@
-class Filter < ActiveRecord::Base
+class Filter
+  include Desk
+  attr_accessor :name, :id
+
+  @@path = "filters"
+
+  def initialize(name, id)
+    @name = name
+    @id = id
+  end
+
+  def self.find(id)
+    response = Desk.request("#{@@path}/#{id}", :get, nil)
+    Filter.new(response['name'], parse_id(response))
+  end
+
+  def self.parse_id(link)
+    #since id doesn't seem to be provided,lets get it off the end of the self link
+    id = link['_links']['self']['href'].to_s.split("/").last
+  end
 end
--- spec/models/filter_spec.rb
+++ spec/models/filter_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
+require 'desk'
 
 RSpec.describe Filter, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  FILTER_ID = "2172115" #this is the test filter
+
+  context "find" do
+    before do
+      @filter = Filter.find(FILTER_ID)
+    end
+
+    it "returns a filter" do
+      expect(@filter.class).to eq Filter
+    end
+
+    it "has a name" do
+      expect(@filter.name).to eq "Test Filter"
+    end
+
+    it "has an id" do
+      expect(@filter.id).to eq FILTER_ID
+    end
+  end
 end
