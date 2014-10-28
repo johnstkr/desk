38fab9644b8a69bd3f0d182e0b58422171849edc
Kelly Johnston <johnstkr@gmail.com>
2014-10-28 17:08:37 -0600

    updates filters.js with the remainder of the js changes

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -12,8 +12,42 @@ $(document).ready(function () {
     }).bind("ajax:error", function (evt, data, status, xhr) {
         $("div#errors").removeClass("hidden");
     });
+
+    $(document).on("ajax:success", ".label-form", function (e, data, status, xhr) {
+        var id = $(this).data("case");
+        update_current_label_list(data, id);
+        //since the form can be reused but the names cannot, clear out the fields
+        $(this).find("input.form-control").val("")
+        //and reset the pulldown too
+        $(this).find("select.form-control").prop('selectedIndex', 0);
+    }).on("ajax:error", function (e, xhr, status, error) {
+        $(".label-result").append("<p>ERROR</p>");
+    });
+
+
+    $(document).on("ajax:success", ".label-add-form", function (e, data, status, xhr) {
+        var id = $(this).data("case");
+        update_current_label_list(data, id);
+
+
+    }).on("ajax:error", function (e, xhr, status, error) {
+        $(".label-result").append("<p>ERROR</p>");
+    });
 });
 
 $(document).on('click', 'a.indicator_trigger', function (event) {
     $(this).find('.indicator').removeClass("hidden");
 });
+
+$(document).on("hidden.bs.modal", "#case-modal", function () {
+    $(this).removeData("bs.modal");
+});
+
+var update_current_label_list = function (data, id) {
+    //add it to the current tags
+    $(".current").append(data);
+    //add it to the case listing too
+    //get the id from data-case
+    //append to the row
+    $("#case-row-" + id).append(data);
+};
